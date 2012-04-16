3f1cba6859556191c59931f8ac04b17528bde2d8
Kriss Watt <kriss.watt@gmail.com>
2012-04-16 12:29:35 +0100

    Added simple response model.

--- discussion.js
+++ discussion.js
@@ -3,16 +3,30 @@ var discussion = {};
 
 discussion.timeline = (function() {
     var init = function() {
-        $('.discussion').on('click', '.responses', getResponses);
+        $('.discussion').on('click', '.responses', simpleDisplay);
     };
 
-    var getResponses = function(event) {
+    var simpleDisplay = function(event) {
         event.preventDefault();
+        var comment = $(this).parents('[data-comment-id]');
 
-        var id = $(this).parents('[data-comment-id]').attr('data-comment-id');
-        var responses = $('[data-response-to=' + id + ']');
-
-        console.log(responses);
+        if (comment.data('showing-responses')) {
+            comment.find('.response').remove();
+            comment.data('showing-responses', false);
+        }
+        else {
+            var id = comment.attr('data-comment-id');
+            var responses = $('[data-response-to=' + id + ']');
+            var i, l, r;
+            for (i = 0, l = responses.length; i < l; i++) {
+                r = $(responses[i]).clone();
+                r.addClass('response').find('.response').remove();
+                r.hide().find('.tools').empty();
+                comment.append(r);
+            }
+            comment.data('showing-responses', true)
+                   .find('.response').fadeIn();
+        }
     };
 
     return {
